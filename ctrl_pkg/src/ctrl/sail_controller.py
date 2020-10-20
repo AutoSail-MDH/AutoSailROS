063aa510df0f69b940303313b9ba0d9495707060
ASB7 <asn14015@student.mdh.se>
2020-10-20 08:36:41 +0200

    add function

--- ctrl_pkg/src/ctrl/sail_controller.py
+++ ctrl_pkg/src/ctrl/sail_controller.py
@@ -1,2 +1,3 @@
 def sail_angle_calculation(wind_sensor_readings):
+    D_sail = np.multiply(-np.sign(W_apparent), (((min(Sail) - max(Sail)) / np.pi) * np.abs(W_apparent) + max(Sail)))
     return sail_angle
