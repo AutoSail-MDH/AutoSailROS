7241441f1821d1eb2b432408682157f9944d9e35
casper-boop <Cag16002@student.mdh.se>
2020-10-20 09:56:28 +0200

    Speed polar diagram and corresponding test.

---
+++ path_planner/test/speed_polar_diagram_test.py
@@ -0,0 +1,44 @@
+import numpy as np
+from path_planner import *
+from path_planner import speed_polar_diagram_function as spdf
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        w_speed = 1
+        w_theta = 90
+        [max_vel, up_beat, dn_beat, w_theta] = spdf.speed_polar_diagram_calculation(w_speed, w_theta)
+        self.assertEqual([max_vel, up_beat, dn_beat], [4.78, 45 + w_theta, 139.4 + w_theta])
+        print(max_vel)
+        print(up_beat)
+        print(dn_beat)
+
+        w_speed = 30
+        w_theta = 30
+        [max_vel, up_beat, dn_beat, w_theta] = spdf.speed_polar_diagram_calculation(w_speed, w_theta)
+        self.assertEqual([max_vel, up_beat, dn_beat], [12.6, 32.6 + w_theta, 148.1 + w_theta])
+        print(max_vel)
+        print(up_beat)
+        print(dn_beat)
+
+        w_speed = 4.6
+        w_theta = 350
+        [max_vel, up_beat, dn_beat, w_theta] = spdf.speed_polar_diagram_calculation(w_speed, w_theta)
+        self.assertEqual([max_vel, up_beat, dn_beat], [4.78, 45 + w_theta, 139.4 + w_theta])
+        print(max_vel)
+        print(up_beat)
+        print(dn_beat)
+
+        w_speed = -4.6
+        w_theta = 350
+        [max_vel, up_beat, dn_beat, w_theta] = spdf.speed_polar_diagram_calculation(w_speed, w_theta)
+        self.assertEqual([max_vel, up_beat, dn_beat], [4.78, 45 + w_theta, 139.4 + w_theta])
+
+        print(max_vel)
+        print(up_beat)
+        print(dn_beat)
+
+
+if __name__ == '__main__':
+    unittest.main()
