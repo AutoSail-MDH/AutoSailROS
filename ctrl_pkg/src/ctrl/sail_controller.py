9ce2474271a3f91ccb0cf87c694753ed1f246a93
AdemSchen <asn14015@student.mdh.se>
2020-10-20 13:51:54 +0200

    Modified from degree to radians.

--- ctrl_pkg/src/ctrl/sail_controller.py
+++ ctrl_pkg/src/ctrl/sail_controller.py
@@ -2,10 +2,11 @@ import numpy as np
 
 
 #  wind_sensor_readings[0] will give wind speed
-#  wind_sensor_readings[1] will give wind angle
+#  wind_sensor_readings[1] will give wind angle in degrees
 def sail_angle_calculation(wind_sensor_readings):
+    #  limits on sail angle
     sail_limits = [-np.pi / 5.2, np.pi / 5.2]
     sail_angle = np.multiply(-np.sign(wind_sensor_readings[1]),
-                             (((min(sail_limits) - max(sail_limits)) / np.pi) * np.abs(wind_sensor_readings[0]) +
-                              max(sail_limits)))
+                             (((min(sail_limits) - max(sail_limits)) / np.pi) *
+                              np.abs(np.deg2rad(wind_sensor_readings[1])) + max(sail_limits)))
     return sail_angle
