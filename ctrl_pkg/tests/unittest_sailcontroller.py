ddec8c5224afd1c8936940a2d44deee7a7259339
AdemSchen <asn14015@student.mdh.se>
2020-10-20 14:49:39 +0200

    Modified from degree to radians.

--- ctrl_pkg/tests/unittest_sailcontroller.py
+++ ctrl_pkg/tests/unittest_sailcontroller.py
@@ -9,12 +9,16 @@ sys.path.append('../scripts/')
 
 class TestSailController(unittest.TestCase):
 
-    #  to sail_angle_calculatipon position 0 takes wind speed, 1 takes wind angle
+    #  to sail_angle_calculation position 0 takes wind speed, 1 takes wind angle
     def test_negative_angle(self):
-        self.assertAlmostEqual(self, sail_angle_calculation([5, 90]), -0)
+        self.assertAlmostEqual(sail_angle_calculation([5, -45]), 0.3020762166913262)
+        self.assertEqual(sail_angle_calculation([5, -90]), 0)
+        self.assertAlmostEqual(sail_angle_calculation([5, -180]), -0.6041524333826525)
 
     def test_positive_angle(self):
-        self.assertEqual(self, sail_angle_calculation([5, -90]), -0)
+        self.assertAlmostEqual(sail_angle_calculation([5, 45]), -0.3020762166913262)
+        self.assertEqual(sail_angle_calculation([5, 90]), 0)
+        self.assertAlmostEqual(sail_angle_calculation([5, 180]), 0.6041524333826525)
 
 
 #  class TestPID(unittest.TestCase):
