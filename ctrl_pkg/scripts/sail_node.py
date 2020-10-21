66794cf232788eee4e84524c7aa2806f4838b557
AdemSchen <asn14015@student.mdh.se>
2020-10-21 16:04:35 +0200

    test to check that publish worked correctly.

--- ctrl_pkg/scripts/sail_node.py
+++ ctrl_pkg/scripts/sail_node.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 import rospy
-rospy.init_node("sail_controller")
-from std_msgs.msg import String
+import std_msgs.msg
+from ctrl.sail_controller import sail_angle_calculation
 
 
 if __name__ == "__main__":
-    sc = sail_controller()
-    rospy.Subscriber(name="sail_control", data_class=std_msgs.msg.Float32, callback=sc.sail_angle_calculation,
+    rospy.init_node("sail_control")
+    pub_testSC = rospy.Publisher("sail_control_topic", std_msgs.msg.Float32, queue_size=1)
+    rate = rospy.Rate(10)
+    rospy.Subscriber(name="sail_control_topic", data_class=std_msgs.msg.Float32, callback=sail_angle_calculation,
                      queue_size=1)
 
-    rospy.init_node("pid")
-    x = 0.1
-    pub_test1 = rospy.Publisher("pathplanner", std_msgs.msg.Float32, queue_size=1)
+    while not rospy.is_shutdown():
+        pub_testSC.publish(100)
+        rate.sleep()
