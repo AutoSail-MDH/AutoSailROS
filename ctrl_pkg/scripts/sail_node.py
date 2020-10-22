846f6a73854ba6ccc941421fdc61f68d5a18d6e5
AdemSchen <asn14015@student.mdh.se>
2020-10-22 15:38:18 +0200

    change subscriber name

--- ctrl_pkg/scripts/sail_node.py
+++ ctrl_pkg/scripts/sail_node.py
@@ -6,11 +6,13 @@ from ctrl.sail_controller import sail_angle_calculation
 
 if __name__ == "__main__":
     rospy.init_node("sail_control")
+    sail_limits = rospy.get_param('~sail_limits')
     pub_testSC = rospy.Publisher("sail_control_topic", std_msgs.msg.Int64, queue_size=1)
     rate = rospy.Rate(10)
-    rospy.Subscriber(name="sail_control_topic2", data_class=std_msgs.msg.Float32, callback=sail_angle_calculation,
-                     queue_size=1)
+ #   rospy.Subscriber(name="sail_control_topic", data_class=std_msgs.msg.Int64,
+ #                       callback=sail_angle_calculation, queue_size=1)
 
     while not rospy.is_shutdown():
-        pub_testSC.publish(100)
+        sail_angle = sail_angle_calculation(90, sail_limits)
+        pub_testSC.publish()
         rate.sleep()
--- ctrl_pkg/src/ctrl/sail_controller.py
+++ ctrl_pkg/src/ctrl/sail_controller.py
@@ -1,14 +1,13 @@
 import numpy as np
-import rospy
 
 
 #  wind_sensor_readings[0] will give wind speed
 #  wind_sensor_readings[1] will give wind angle in degrees
-def sail_angle_calculation(wind_sensor_readings):
+def sail_angle_calculation(wind_sensor_readings, sail_limits):
     #  limits on sail angle
     #  sail_limits = [-np.pi / 5.2, np.pi / 5.2]
-    if -180 < wind_sensor_readings < 180:
-        sail_limits = rospy.get_param('~sail_limits')
+    sail_angle_rad = 0
+    if -181 < int(wind_sensor_readings) < 181:
         sail_angle_rad = np.multiply(-np.sign(wind_sensor_readings),
                                      (((float(min(sail_limits)) - float(max(sail_limits))) / np.pi) *
                                       np.abs(np.deg2rad(wind_sensor_readings)) + max(sail_limits)))
