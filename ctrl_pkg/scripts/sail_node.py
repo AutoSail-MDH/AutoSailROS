99a9669f3c24f8a890324369f2871263c81a6cea
AdemSchen <asn14015@student.mdh.se>
2020-10-22 13:06:21 +0200

    change subscriber name

--- ctrl_pkg/scripts/sail_node.py
+++ ctrl_pkg/scripts/sail_node.py
@@ -6,9 +6,9 @@ from ctrl.sail_controller import sail_angle_calculation
 
 if __name__ == "__main__":
     rospy.init_node("sail_control")
-    pub_testSC = rospy.Publisher("sail_control_topic", std_msgs.msg.Float32, queue_size=1)
+    pub_testSC = rospy.Publisher("sail_control_topic", std_msgs.msg.Int64, queue_size=1)
     rate = rospy.Rate(10)
-    rospy.Subscriber(name="sail_control_topic", data_class=std_msgs.msg.Float32, callback=sail_angle_calculation,
+    rospy.Subscriber(name="sail_control_topic2", data_class=std_msgs.msg.Float32, callback=sail_angle_calculation,
                      queue_size=1)
 
     while not rospy.is_shutdown():
