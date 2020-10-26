4c46d0a3a1fd264f756bed6684574fc7f935d879
jnn16012 <jnn16012@student.mdh.se>
2020-10-26 08:55:38 +0100

    commented out the plots in the redder unittest

--- ctrl_pkg/tests/unittest_rudder_controller.py
+++ ctrl_pkg/tests/unittest_rudder_controller.py
@@ -121,9 +121,9 @@ class TestPID(unittest.TestCase):
                 run_convergence(self.pid, heading, _time, start_time, setpoint)
 
         # Uncomment for plotting of heading
-        plt.plot(_time, heading, label='measured')
-        plt.plot(_time, setpoint, label='target')
-        plt.show()
+        # plt.plot(_time, heading, label='measured')
+        # plt.plot(_time, setpoint, label='target')
+        # plt.show()
 
     def test_PID_controller_switch(self):
         # Initial previous if use heading
