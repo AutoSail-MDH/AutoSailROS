1f37ead1c2e36c9d46b08f081c16df11a8e8cbba
AdemSchen <asn14015@student.mdh.se>
2020-10-29 14:35:20 +0100

    add test for sail trimming

--- ctrl_pkg/tests/unittest_sailcontroller.py
+++ ctrl_pkg/tests/unittest_sailcontroller.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 import unittest
-import sys
 
-from ctrl.sail_controller import sail_angle_calculation
-#  from ctrl import rudder_controller
-PKG = 'package'
+from ctrl.sail_controller import calculate_sail_angle
+from ctrl.sail_controller import trim_sail
 
 
 class TestSailController(unittest.TestCase):
 
     def test_negative_angle(self):
         sail_limits = 0.60415243338
-        self.assertAlmostEqual(sail_angle_calculation(-45, sail_limits), 0.30207621669)
-        self.assertEqual(sail_angle_calculation(-90, sail_limits), 0)
-        self.assertAlmostEqual(sail_angle_calculation(-180, sail_limits), -0.60415243338)
+        self.assertAlmostEqual(calculate_sail_angle(-45, sail_limits), 0.30207621669)
+        self.assertEqual(calculate_sail_angle(-90, sail_limits), 0)
+        self.assertAlmostEqual(calculate_sail_angle(-180, sail_limits), -0.60415243338)
 
     def test_positive_angle(self):
         sail_limits = 0.60415243338
-        self.assertAlmostEqual(sail_angle_calculation(45, sail_limits), -0.30207621669)
-        self.assertEqual(sail_angle_calculation(90, sail_limits), 0)
-        self.assertAlmostEqual(sail_angle_calculation(180, sail_limits), 0.60415243338)
+        self.assertAlmostEqual(calculate_sail_angle(45, sail_limits), -0.30207621669)
+        self.assertEqual(calculate_sail_angle(90, sail_limits), 0)
+        self.assertAlmostEqual(calculate_sail_angle(180, sail_limits), 0.60415243338)
+
+    def sail_trimming_test(self):
+        self.assertEqual(trim_sail(0), 1620)
+        self.assertEqual(trim_sail(80), 0)
 
 
 if __name__ == '__main__':
