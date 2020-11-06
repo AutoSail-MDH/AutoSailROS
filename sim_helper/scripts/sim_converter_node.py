79d5a2f0fd05e357b4814e1cb59172695bbc1d7d
jpdf <pfd16001@student.mdh.se>
2020-11-06 15:37:58 +0100

    Add delay to rudder

--- sim_helper/scripts/sim_converter_node.py
+++ sim_helper/scripts/sim_converter_node.py
@@ -5,11 +5,15 @@ import rospy
 from std_msgs.msg import Float64
 from gazebo_msgs.msg import ModelStates
 from geometry_msgs.msg import TwistWithCovarianceStamped, Vector3Stamped
-from sensor_msgs.msg import Imu
+from sensor_msgs.msg import Imu, NavSatFix
 
 imu_pub = rospy.Publisher("gps/navheading", Imu, queue_size=1)
 twist_pub = rospy.Publisher("gps/fix_velocity", TwistWithCovarianceStamped, queue_size=1)
 wind_pub = rospy.Publisher("wind/apparent_rad", Float64, queue_size=1)
+pos_pub = rospy.Publisher("gps/fix", NavSatFix, queue_size=1)
+rudder_pub = rospy.Publisher("/rudder_position/command", Float64, queue_size=1)
+# GPS fix position, imu quartenions, wind speed
+
 
 
 def model_state_callback(data):
@@ -35,6 +39,14 @@ def wind_callback(data):
     wind_pub.publish(wind_angle)
 
 
+desired_rudder_angle = 0.
+
+
+def rudder_callback(data):
+    global desired_rudder_angle
+    desired_rudder_angle = data.data
+
+
 if __name__ == "__main__":
     rospy.init_node("sim_converter")
 
@@ -43,4 +55,15 @@ if __name__ == "__main__":
                      callback=model_state_callback, queue_size=1)
     rospy.Subscriber(name="wind/apparent", data_class=Vector3Stamped,
                      callback=wind_callback, queue_size=1)
+    rospy.Subscriber(name="rudder_controller/rudder_angle", data_class=Float64, callback=rudder_callback, queue_size=1)
+    r = rospy.Rate(260)
+    current_rudder_angle = 0.
+    onedeginrad = math.pi/180.
+    while not rospy.is_shutdown():
+        if abs(desired_rudder_angle-current_rudder_angle) <= onedeginrad:
+            current_rudder_angle = desired_rudder_angle
+        else:
+            current_rudder_angle += math.copysign(onedeginrad, desired_rudder_angle-current_rudder_angle)
+        rudder_pub.publish(current_rudder_angle)
+        r.sleep()
     rospy.spin()
