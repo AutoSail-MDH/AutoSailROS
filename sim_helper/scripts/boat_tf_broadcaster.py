2d036f4b73156432c5919253d7ec2b5e9ba43a9c
jpdf <pfd16001@student.mdh.se>
2020-11-18 10:52:47 +0100

    Add node to publish a tf frame of the boat for mapviz usage

---
+++ sim_helper/scripts/boat_tf_broadcaster.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2.7
+import rospy
+import tf
+import tf.transformations
+from nav_msgs.msg import Odometry
+
+
+def handle_boat_pose(msg):
+    br = tf.TransformBroadcaster()
+    br.sendTransform((msg.pose.pose.position.x, msg.pose.pose.position.y, 0),
+                     tf.transformations.quaternion_from_euler(0, 0, 0),
+                     rospy.Time.now(),
+                     'boat',
+                     'map')
+
+
+if __name__ == '__main__':
+    rospy.init_node('boat_tf_broadcaster')
+    rospy.Subscriber('/odom', Odometry, handle_boat_pose)
+    rospy.spin()
