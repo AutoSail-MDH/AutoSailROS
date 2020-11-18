812df194965b86cec89bc091faa1ed273d2b9830
jpdf <pfd16001@student.mdh.se>
2020-11-18 10:51:19 +0100

    Remove unnecessary publishers

--- sim_helper/scripts/sim_converter_node.py
+++ sim_helper/scripts/sim_converter_node.py
@@ -7,30 +7,16 @@ from gazebo_msgs.msg import ModelStates
 from geometry_msgs.msg import TwistWithCovarianceStamped, Vector3Stamped
 from sensor_msgs.msg import Imu, NavSatFix
 
-imu_pub = rospy.Publisher("gps/navheading", Imu, queue_size=1)
-twist_pub = rospy.Publisher("gps/fix_velocity", TwistWithCovarianceStamped, queue_size=1)
-wind_pub = rospy.Publisher("wind/apparent_rad", Float64, queue_size=1)
-pos_pub = rospy.Publisher("gps/fix", NavSatFix, queue_size=1)
 rudder_pub = rospy.Publisher("/rudder_position/command", Float64, queue_size=1)
 sail_pub = rospy.Publisher("/main_sail_position/command", Float64, queue_size=1)
-# GPS fix position, imu quartenions, wind speed
+gps_vel_pub = rospy.Publisher("/gps/fix_velocity", TwistWithCovarianceStamped, queue_size=1)
 
 
-def model_state_callback(data):
-    global imu_pub, twist_pub
-    i = data.name.index("rs750")
-    linear = data.twist[i].linear
-    angular = data.twist[i].angular
-    q = data.pose[i].orientation
-
-    imu_msg = Imu()
-    imu_msg.orientation = q
-    imu_pub.publish(imu_msg)
-
-    twist_msg = TwistWithCovarianceStamped()
-    twist_msg.twist.twist.linear = linear
-    twist_msg.twist.twist.angular = angular
-    twist_pub.publish(twist_msg)
+def gps_velocity_callback(data):
+    global gps_vel_pub
+    vel_msg = TwistWithCovarianceStamped()
+    vel_msg.twist.twist.linear = data.vector
+    gps_vel_pub.publish(vel_msg)
 
 
 wind_angle = 0
@@ -40,7 +26,6 @@ def wind_callback(data):
     global wind_pub
     global wind_angle
     wind_angle = math.atan2(data.vector.y, data.vector.x)
-    wind_pub.publish(wind_angle)
 
 
 desired_rudder_angle = 0.
@@ -62,12 +47,11 @@ if __name__ == "__main__":
     rospy.init_node("sim_converter")
 
     # Subscribes
-    rospy.Subscriber(name="gazebo/model_states", data_class=ModelStates,
-                     callback=model_state_callback, queue_size=1)
     rospy.Subscriber(name="wind/apparent", data_class=Vector3Stamped,
                      callback=wind_callback, queue_size=1)
     rospy.Subscriber(name="rudder_controller/rudder_angle", data_class=Float64, callback=rudder_callback, queue_size=1)
     rospy.Subscriber(name="sail_controller/sail_angle", data_class=Float64, callback=sail_callback, queue_size=1)
+    rospy.Subscriber("/sensors/gps_velocity", Vector3Stamped, gps_velocity_callback, queue_size=1)
 
     r = rospy.Rate(260)
     current_rudder_angle = 0.
