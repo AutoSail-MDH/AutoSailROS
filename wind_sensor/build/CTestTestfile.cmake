546c6b2b08374f5897d48da421029e57763e9608
MrSurfinbird <sme15002@student.mdh.se>
2020-11-20 10:38:33 +0100

    Removed the build and devel folders.

--- wind_sensor/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/asb6/PycharmProjects/AutoSailROS/wind_sensor/src
-# Build directory: /home/asb6/PycharmProjects/AutoSailROS/wind_sensor/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("wind_sensor")
--- wind_sensor/build/wind_sensor/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/asb6/PycharmProjects/AutoSailROS/wind_sensor/src/wind_sensor
-# Build directory: /home/asb6/PycharmProjects/AutoSailROS/wind_sensor/build/wind_sensor
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
