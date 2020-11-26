9a1378e46369baf1c4004c36451676fa0ff093c1
patrik <patrik.forsslund@gmail.com>
2020-11-26 14:53:23 +0100

    Fix dynamic config error in sail_node.py

--- autosail/scripts/ctrl/sail_node.py
+++ autosail/scripts/ctrl/sail_node.py
@@ -11,7 +11,7 @@ from ctrl.sail_controller import trim_sail
 
 # Dynamic configuration imports
 from dynamic_reconfigure.server import Server
-from autosail.cfg import SailController
+from autosail.cfg import SailControllerConfig
 
 
 # Class for saving values from the subscriptions
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     sc_pid = pid.PID()
 
     # Dynamic reconfigure setup
-    srv = Server(SailController, dynamic_reconf_callback)
+    srv = Server(SailControllerConfig, dynamic_reconf_callback)
 
     while not rospy.is_shutdown():
         # Use the PID to get a desired roll angle
