c5474db0856cd4c705991fa79b0617b2bfb357d8
DavidPantzar <david@pantzar.dev>
2020-12-02 12:10:18 +0100

    Create 3 Iterations of unittest_wind_sensor

--- autosail/test/wind_sensor/unittest_wind_sensor.py
+++ autosail/test/wind_sensor/unittest_wind_sensor.py
@@ -8,34 +8,59 @@ import tempfile
 import os
 import math
 
-y = 0
 
-mock_wind_speed_val = [0.001, 0.001, 0.001]
-mock_wind_angle_val = [1.2042771838760873, 1.2042771838760873, 1.2042771838760873]
-mock_temp = [299.15, 299.15, 299.15]
-mock_battery = [0.2, 0.2, 0.2]
+mock_wind_speed_val = [1.145678, 1.26456, 1.35834]
+mock_wind_angle_val = [1.1042771838760873, 1.2042771838760873, 1.3042771838760873]
+mock_temp = [21, 22, 23]
+mock_battery = [0.1, 0.2, 0.3]
 mock_roll = [0.05235987755982988, 0.05235987755982988, 0.05235987755982988]
 mock_pitch = [0.5410520681182421, 0.5410520681182421, 0.5410520681182421]
 mock_yaw = [1.780235837034216, 1.780235837034216, 1.780235837034216]
 
-mock_wind_vector = [math.cos(mock_wind_angle_val[y]) * mock_wind_speed_val[y],
-                    math.sin(mock_wind_angle_val[y]) * mock_wind_speed_val[y]]
 
+def write_mock_values(x):
 
-def write_mock_values():
+    if x == 0:
+        return b'GOT MESSAGE: {"updates":[{"source":{"label":"Calypso Ultrasonic","type":"Ultrasonic"},' \
+               b'"timestamp":"2020-12-01T12:17:22.723Z","values":[' \
+               b'{"path":"environment.outside.temperature","value":21},' \
+               b'{"path":"environment.wind.angleApparent","value": 1.1042771838760873},' \
+               b'{"path":"environment.wind.speedApparent","value":1.145678},' \
+               b'{"path":"electrical.batteries.99.name","value":"ULTRASONIC"},' \
+               b'{"path":"electrical.batteries.99.location","value":"Mast"},' \
+               b'{"path":"electrical.batteries.99.capacity.stateOfCharge","value":0.1},' \
+               b'{"path":"navigation.attitude.roll","value":0.05235987755982988},' \
+               b'{"path":"navigation.attitude.pitch","value":0.5410520681182421},' \
+               b'{"path":"navigation.attitude.yaw","value":1.780235837034216},' \
+               b'{"path":"navigation.headingMagnetic","value":1.780235837034216}]}]}\n'
 
-    return b'GOT MESSAGE: {"updates":[{"source":{"label":"Calypso Ultrasonic","type":"Ultrasonic"},' \
-           b'"timestamp":"2020-12-01T12:17:22.723Z","values":[' \
-           b'{"path":"environment.outside.temperature","value":299.15},' \
-           b'{"path":"environment.wind.angleApparent","value": 1.2042771838760873},' \
-           b'{"path":"environment.wind.speedApparent","value":0},' \
-           b'{"path":"electrical.batteries.99.name","value":"ULTRASONIC"},' \
-           b'{"path":"electrical.batteries.99.location","value":"Mast"},' \
-           b'{"path":"electrical.batteries.99.capacity.stateOfCharge","value":0.2},' \
-           b'{"path":"navigation.attitude.roll","value":0.05235987755982988},' \
-           b'{"path":"navigation.attitude.pitch","value":0.5410520681182421},' \
-           b'{"path":"navigation.attitude.yaw","value":1.780235837034216},' \
-           b'{"path":"navigation.headingMagnetic","value":1.780235837034216}]}]}\n'
+    if x == 1:
+        return b'GOT MESSAGE: {"updates":[{"source":{"label":"Calypso Ultrasonic","type":"Ultrasonic"},' \
+               b'"timestamp":"2020-12-01T12:17:22.723Z","values":[' \
+               b'{"path":"environment.outside.temperature","value":22},' \
+               b'{"path":"environment.wind.angleApparent","value": 1.2042771838760873},' \
+               b'{"path":"environment.wind.speedApparent","value":1.26456},' \
+               b'{"path":"electrical.batteries.99.name","value":"ULTRASONIC"},' \
+               b'{"path":"electrical.batteries.99.location","value":"Mast"},' \
+               b'{"path":"electrical.batteries.99.capacity.stateOfCharge","value":0.2},' \
+               b'{"path":"navigation.attitude.roll","value":0.05235987755982988},' \
+               b'{"path":"navigation.attitude.pitch","value":0.5410520681182421},' \
+               b'{"path":"navigation.attitude.yaw","value":1.780235837034216},' \
+               b'{"path":"navigation.headingMagnetic","value":1.780235837034216}]}]}\n'
+
+    if x == 2:
+        return b'GOT MESSAGE: {"updates":[{"source":{"label":"Calypso Ultrasonic","type":"Ultrasonic"},' \
+               b'"timestamp":"2020-12-01T12:17:22.723Z","values":[' \
+               b'{"path":"environment.outside.temperature","value":23},' \
+               b'{"path":"environment.wind.angleApparent","value": 1.3042771838760873},' \
+               b'{"path":"environment.wind.speedApparent","value":1.35834},' \
+               b'{"path":"electrical.batteries.99.name","value":"ULTRASONIC"},' \
+               b'{"path":"electrical.batteries.99.location","value":"Mast"},' \
+               b'{"path":"electrical.batteries.99.capacity.stateOfCharge","value":0.3},' \
+               b'{"path":"navigation.attitude.roll","value":0.05235987755982988},' \
+               b'{"path":"navigation.attitude.pitch","value":0.5410520681182421},' \
+               b'{"path":"navigation.attitude.yaw","value":1.780235837034216},' \
+               b'{"path":"navigation.headingMagnetic","value":1.780235837034216}]}]}\n'
 
 
 class TestWindSensor(TestCase):
@@ -54,7 +79,7 @@ class TestWindSensor(TestCase):
     def test_battery_charge(self):
         x = 0
         while True:
-            mock_values = write_mock_values()
+            mock_values = write_mock_values(x)
             self.stdout_mock.write(mock_values)
             self.stdout_mock.seek(0)
             battery_charge = self.ws.get_battery_charge()
@@ -63,10 +88,14 @@ class TestWindSensor(TestCase):
             if x > 2:
                 break
 
+    def mock_wind_vector(self, x):
+        return [math.cos(mock_wind_angle_val[x]) * mock_wind_speed_val[x],
+                math.sin(mock_wind_angle_val[x]) * mock_wind_speed_val[x]]
+
     def test_get_rpy(self):
         x = 0
         while True:
-            mock_values = write_mock_values()
+            mock_values = write_mock_values(x)
             self.stdout_mock.write(mock_values)
             self.stdout_mock.seek(0)
             rpy_vector = self.ws.get_rpy()
@@ -80,7 +109,7 @@ class TestWindSensor(TestCase):
     def test_get_tmp(self):
         x = 0
         while True:
-            mock_values = write_mock_values()
+            mock_values = write_mock_values(x)
             self.stdout_mock.write(mock_values)
             self.stdout_mock.seek(0)
             temp = self.ws.get_temp()
@@ -90,15 +119,18 @@ class TestWindSensor(TestCase):
                 break
 
     def test_wind_vector(self):
-        mock_values = write_mock_values()
-        self.stdout_mock.write(mock_values)
-        self.stdout_mock.seek(0)
-        wind_vector = self.ws.get_wind_vector()
-        self.assertEqual(mock_wind_vector, wind_vector)
-
+        x = 0
+        while True:
+            mock_values = write_mock_values(x)
+            self.stdout_mock.write(mock_values)
+            self.stdout_mock.seek(0)
+            wind_vector = self.ws.get_wind_vector()
+            self.assertEqual(self.mock_wind_vector(x), wind_vector)
+            x = x + 1
+            if x > 2:
+                break
 
 
 if __name__ == '__main__':
     rosunit.unitrun("autosail", "unittest_wind_sensor", TestWindSensor)
-
 
