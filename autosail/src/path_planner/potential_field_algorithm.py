93231ae0e92cc71324b9fa60c637006639bf6166
jpdf <pfd16001@student.mdh.se>
2020-12-02 17:29:48 +0100

    Refactor plot_heat_map to make it callable

--- autosail/src/path_planner/potential_field_algorithm.py
+++ autosail/src/path_planner/potential_field_algorithm.py
@@ -1,11 +1,4 @@
 import numpy as np
-import math
-from scipy.spatial import distance
-from path_planner import path_planner as pl
-import rospy
-import time
-
-radius_earth = 6371
 
 
 class PointClass:
@@ -61,6 +54,7 @@ class PotentialField:
         self.p_ngz = p_ngz
         self.p_hyst = p_hyst
         self.g_v = g_v
+        self.profile = None
 
     def find_nearest(self, array, value):
         """
@@ -170,8 +164,8 @@ class PotentialField:
         Calculates the potential for a point relative the wind
         :param w_theta: Wind angle
         :param p: current point
-        :param heading:Heading of the vessel
-        :return:
+        :param heading: Heading of the vessel
+        :return: the correct potential of the point
         """
         # calculate max velocity, no-go zones and wind angle
         [max_vel, up_beat, dn_beat, w_theta] = self._speed_polar_diagram_calculation(w_theta, w_speed)
@@ -285,38 +279,23 @@ class PotentialField:
         min_angle, min_index = self._find_global_minima_angle(profile)
         return min_angle, profile
 
-    # def _obstacle_calc(self, p_0, p_1, obstacles):
-    #     """
-    #     calculates the local x,y for the obstacles in the frame of the reference points
-    #     :param p_0: reference point 0
-    #     :param p_1: reference point 1
-    #     :param obstacles: array of obstacles
-    #     :return:
-    #     """
-    #     length_obstacles = np.size(obstacles)
-    #     obstacles_xy_array = np.zeros(shape=(length_obstacles, 2))
-    #     for i in range(length_obstacles):
-    #         obstacles_xy_1d = pl.latlng_to_screen_xy(obstacles[i].latitude, obstacles[i].longitude, p_0, p_1)
-    #         obstacles_xy_array[i, 0] = round(obstacles_xy_1d[0])
-    #         obstacles_xy_array[i, 1] = round(obstacles_xy_1d[1])
-    #     return obstacles_xy_array
-
-    def plot_heat_map(self, profile, heading):
+    def plot_heat_map(self, sleep, heading=None):
         """
         method used to plot the heat map
         profile: profile for the vessel
-        :return:
         """
         import matplotlib.pyplot as plt
-        heading_x = np.linspace(self.diameter / 2 - 1 / 2, self.diameter / 2 + heading[1] * 10, 10)
-        heading_y = np.linspace(self.diameter / 2 - 1 / 2, self.diameter / 2 - heading[0] * 10, 10)
-        profile_matrix = self._reshape_profile(profile)
-        plt.imshow(profile_matrix, cmap='hot', interpolation='nearest')
-        plt.plot(heading_x, heading_y, linewidth=2)
-        plt.ion()
-        plt.show()
-        plt.pause(0.001)
-        plt.clf()
+        if self.profile is not None:
+            profile_matrix = self._reshape_profile(self.profile)
+            plt.imshow(profile_matrix, cmap='hot', interpolation='nearest')
+            if heading is not None:
+                heading_x = np.linspace(self.diameter / 2 - 1 / 2, self.diameter / 2 + heading[1] * 25, 10)
+                heading_y = np.linspace(self.diameter / 2 - 1 / 2, self.diameter / 2 - heading[0] * 25, 10)
+                plt.plot(heading_x, heading_y, linewidth=3)
+            plt.ion()
+            plt.show()
+            plt.pause(sleep)
+            plt.clf()
 
     def calc_heading(self, goal, heading, w_speed, w_theta, position_v, obstacles, v_v):
         """
@@ -337,7 +316,6 @@ class PotentialField:
         obstacles = np.array(obstacles)
         min_angle, profile = self._calculate_profile(position_v, obstacles, goal_pos,
                                                      w_theta, heading, w_speed, v_v)
-        # plot a heat map of the profile for the vessel
-        self.plot_heat_map(profile, heading)
+        self.profile = profile
 
         return min_angle
