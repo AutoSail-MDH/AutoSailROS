20482a8b5cb90403b5f800e8e963a9dbf86d22ad
jnn16012 <jnn16012@student.mdh.se>
2020-12-10 11:39:18 +0100

    Change the distance calculation to float.

--- autosail/scripts/camera/camera_node.py
+++ autosail/scripts/camera/camera_node.py
@@ -46,9 +46,9 @@ if __name__ == "__main__":
         x, y = detect_contour(image)
 
         err, point_cloud_value = point_cloud.get_value(x, y)
-        distance = math.sqrt(point_cloud_value[0] * point_cloud_value[0] +
+        distance = float(math.sqrt(point_cloud_value[0] * point_cloud_value[0] +
                              point_cloud_value[1] * point_cloud_value[1] +
-                             point_cloud_value[2] * point_cloud_value[2])/1000
+                             point_cloud_value[2] * point_cloud_value[2])/1000.)
 
         point_cloud_np = point_cloud.get_data()
         point_cloud_np.dot(tr_np)
