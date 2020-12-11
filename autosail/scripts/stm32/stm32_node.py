3843e9ab675e87badf587654ffb5455e2db30be3
AdemSchen <asn14015@student.mdh.se>
2020-12-11 09:12:52 +0100

    STM32 stuff

--- autosail/scripts/stm32/stm32_node.py
+++ autosail/scripts/stm32/stm32_node.py
@@ -5,6 +5,7 @@ import numpy as np
 from stm32.stm32_handle import sensor_readings
 from autosail.msg import stm32_msg
 
+
 class SubscriberValues:
     def __init__(self):
         self.desired_angle = 1.0
@@ -27,16 +28,6 @@ if __name__ == "__main__":
     stm32_sensors = rospy.Publisher("stm32_handle/sensor_readings", stm32_msg, queue_size=queue_size)
 
     #  subscriber wind sensor readings
-    #rospy.Subscriber(name="sail_control_topic", data_class=std_msgs.msg.Float32, callback=values.callback_SailAngle,
-    #                 queue_size=queue_size)
-
-    #   ADCCurrent = data(2)+data(1)
-    #   I2C[0] = data(4) + data(3)
-    #   I2C[1] = data(6) + data(5)
-    #   I2C[2] = data(8) + data(7)
-    #   WaterDetect1 = data(9)
-    #   WaterDetect2 = data(10)
-    #   pump = data(11)
 
     sensor_send = stm32_msg()
     while not rospy.is_shutdown():
@@ -48,9 +39,10 @@ if __name__ == "__main__":
         sensor_send.I2c_current_1 = sensor_values[3] + sensor_values[2]
         sensor_send.I2c_current_2 = sensor_values[5] + sensor_values[4]
         sensor_send.I2c_current_3 = sensor_values[7] + sensor_values[6]
-        sensor_send.water_detect_1 = sensor_values[8]
-        sensor_send.water_detect_2 = sensor_values[9]
-        sensor_send.pump = sensor_values[10]
+        sensor_send.Battery = sensor_values[9] + sensor_values[8]
+        sensor_send.water_detect_1 = sensor_values[10]
+        sensor_send.water_detect_2 = sensor_values[11]
+        sensor_send.pump = sensor_values[12]
 
         stm32_sensors.publish(sensor_send)
         rate.sleep()
--- autosail/src/stm32/stm32_handle.py
+++ autosail/src/stm32/stm32_handle.py
@@ -5,22 +5,21 @@ import serial.tools.list_ports
 
 def sensor_readings():
     """
-    Communicating to MCU to acquire sensor  data, from water level, current consumption and water detection
+    Communicating to MCU to acquire sensor  data, from battery life, current consumption and water detection
     :return: data from MCU sensors
     """
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=2)  # open serial port
+    ser = serial.Serial('/dev/ttyACM0', 52000, timeout=2)  # open serial port
     if ser.isOpen():
         print("Already open")
     else:
         ser.open()
-    data = b'4'  # data sent
+    data = b'5'  # data sent
     ser.write(data)  # Serial port write data
     while True:
         data = ser.readline()
         if data != b"":
             break
     print("receive data is:", list(data))
-
     ser.close()
     return data
 
