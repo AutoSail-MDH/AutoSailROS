27e757f844ae370a7d002ffde04f7badf4403646
MrSurfinbird <sme15002@student.mdh.se>
2020-12-17 16:34:26 +0100

    Modify probe node

--- autosail/test/probe_node.py
+++ autosail/test/probe_node.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import time
 import rospy
 import threading
 from utils.polls import poll_connections
@@ -11,59 +12,55 @@ import rostopic
 
 class PublishTest:
 
-    def __init__(self, topic: str, timeout: float):
+    def __init__(self, topic: str):
         self.topic = topic
-        self.timeout = timeout
         self.last_time = rospy.Time.now().secs
+        self.msg_received = 0
+        self.subscribed = 0
+        self.msg = None
+        self.last_msg = None
 
-    def poll_publisher(self, topic_type):
-        msg = None
-        try:
-            msg = rospy.wait_for_message(self.topic, topic_type, self.timeout)
-        except ROSException as e:
-            rospy.logerr(f"{self.topic} did not publish any msgs for {self.timeout}s")
-        return msg
 
-    def probe(self):
-        rate = rospy.Rate(1)
-        counter = 0
-        topic_type = rostopic.get_topic_class(topic["name"])[0]
-        while topic_type is None:
-            if counter > 60 or counter == 0:
-                rospy.logerr(f"Could not find topic {self.topic}")
-                counter = 0
-            topic_type = rostopic.get_topic_class(topic["name"])[0]
-            rate.sleep()
-            counter += 1
-            if rospy.is_shutdown():
-                return
-        self.last_msg = topic_type()
-        rate = rospy.Rate(0.5)
+    def subscribe(self):
+        if self.subscribed:
+            return
+        topic_type = rostopic.get_topic_class(self.topic)[0]
+        if topic_type is None:
+            rospy.logerr(f"Could not find topic {self.topic}")
+            return
+        rospy.Subscriber(self.topic, topic_type, self.callback, queue_size=10)
         rospy.loginfo(f"Started probin {self.topic}")
-        while not rospy.is_shutdown():
-            time = rospy.Time.now().secs
-            msg = self.poll_publisher(topic_type)
-            if hasattr(msg, 'header'):
-                delattr(msg, 'header')
-            if msg is not None and msg == self.last_msg:
-                rospy.logwarn(f"{self.topic} did not change since last check, {time - self.last_time}s ago")
-            rate.sleep()
+        self.subscribed = 1
+
+    def callback(self, msg):
+        self.msg = msg
+        self.msg_received = 1
 
 
 if __name__ == "__main__":
     rospy.init_node("test_node", anonymous=True)
 
     topics = rospy.get_param("~topics")
+    rospy.logerr(topics)
+    pub_tests = []
     for topic in topics:
-        pub_test = PublishTest(topic["name"], topic["timeout"])
-        thr = threading.Thread(target=pub_test.probe)
-        thr.start()
-    rospy.spin()
+        pub_tests.append(PublishTest(topic["name"]))
 
-    #wind_pub_test.test_connections(2)
-    #imu_pub_test.test_connections(4)
-    #fix_pub_test.test_connections(1)
-    #fix_vel_pub_test.test_connections(3)
-
-    #rudder_pub_test.test_connections(1)
-    #sail_pub_test.test_connections(1)
+    rate = rospy.Rate(0.5)
+    last_time = time.time()
+    while not rospy.is_shutdown():
+        time_now = time.time()
+        for pub_test in pub_tests:
+            pub_test.subscribe()
+            if not pub_test.msg_received:
+                rospy.logerr(f"{pub_test.topic} did not publish any msgs for {time_now - last_time}s")
+            else:
+                msg = pub_test.msg
+                if hasattr(msg, 'header'):
+                    delattr(msg, 'header')
+                if pub_test is not None and msg == pub_test.last_msg:
+                    rospy.logwarn(f"{pub_test.topic} did not change since last check, {time_now - last_time}s ago")
+            pub_test.last_msg = pub_test.msg
+            pub_test.msg_received = 0
+        last_time = time_now
+        rate.sleep()
