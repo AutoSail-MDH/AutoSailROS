24b31537a6097323234ed2158ddc749dcba9cefb
MrSurfinbird <sme15002@student.mdh.se>
2020-12-17 17:23:53 +0100

    Modify probe_node to give the actual time since last message

--- autosail/test/probe_node.py
+++ autosail/test/probe_node.py
@@ -19,6 +19,7 @@ class PublishTest:
         self.subscribed = 0
         self.msg = None
         self.last_msg = None
+        self.last_msg_time = time.time()
 
 
     def subscribe(self):
@@ -35,32 +36,31 @@ class PublishTest:
     def callback(self, msg):
         self.msg = msg
         self.msg_received = 1
+        self.last_msg_time = time.time()
 
 
 if __name__ == "__main__":
     rospy.init_node("test_node", anonymous=True)
 
     topics = rospy.get_param("~topics")
-    rospy.logerr(topics)
     pub_tests = []
     for topic in topics:
         pub_tests.append(PublishTest(topic["name"]))
 
     rate = rospy.Rate(0.5)
-    last_time = time.time()
     while not rospy.is_shutdown():
         time_now = time.time()
         for pub_test in pub_tests:
             pub_test.subscribe()
             if not pub_test.msg_received:
-                rospy.logerr(f"{pub_test.topic} did not publish any msgs for {time_now - last_time}s")
+                rospy.logerr(f"{pub_test.topic} did not publish any msgs for {time_now - pub_test.last_msg_time}s")
             else:
                 msg = pub_test.msg
                 if hasattr(msg, 'header'):
                     delattr(msg, 'header')
                 if pub_test is not None and msg == pub_test.last_msg:
-                    rospy.logwarn(f"{pub_test.topic} did not change since last check, {time_now - last_time}s ago")
+                    rospy.logwarn(f"""{pub_test.topic} did not change since last check, 
+                                  {time_now - pub_test.last_msg_time}s ago""")
             pub_test.last_msg = pub_test.msg
             pub_test.msg_received = 0
-        last_time = time_now
         rate.sleep()
