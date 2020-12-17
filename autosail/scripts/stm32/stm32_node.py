0ba3042ddcb26e48c846e6bf2c355a7cfafd9d5d
Tx2One <david@pantzar.dev>
2020-12-17 14:47:47 +0100

    Modify stm32 to handle timeouts

--- autosail/scripts/stm32/stm32_node.py
+++ autosail/scripts/stm32/stm32_node.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     queue_size = rospy.get_param("~queue_size", 60)
     predefined_rate = rospy.get_param("~rate", 60)
     rate = rospy.Rate(predefined_rate)
+    timeout = rospy.get_param("~timeout", 1)
 
     #  Publisher
     stm32_sensors = rospy.Publisher("stm32_handle/sensor_readings", stm32_msg, queue_size=queue_size)
@@ -21,18 +22,20 @@ if __name__ == "__main__":
     openSTM32Serial(rospy.get_param("~port", "/dev/ttyACM60"))
     while not rospy.is_shutdown():
         # Get sensor values from stm32
-        sensor_values = sensor_readings()
-
-        # position the sensor values correct
-        sensor_send.adc_current = sensor_values[1] + sensor_values[0]
-        sensor_send.I2c_current_1 = sensor_values[3] + sensor_values[2]
-        sensor_send.I2c_current_2 = sensor_values[5] + sensor_values[4]
-        sensor_send.I2c_current_3 = sensor_values[7] + sensor_values[6]
-        sensor_send.Battery = sensor_values[9] + sensor_values[8]
-        sensor_send.water_detect_1 = sensor_values[10]
-        sensor_send.water_detect_2 = sensor_values[11]
-        sensor_send.pump = sensor_values[12]
-
+        sensor_values = sensor_readings(timeout=timeout)
+        if sensor_values:
+            # position the sensor values correct
+            sensor_send.adc_current = sensor_values[1] + sensor_values[0]
+            sensor_send.I2c_current_1 = sensor_values[3] + sensor_values[2]
+            sensor_send.I2c_current_2 = sensor_values[5] + sensor_values[4]
+            sensor_send.I2c_current_3 = sensor_values[7] + sensor_values[6]
+            sensor_send.Battery = sensor_values[9] + sensor_values[8]
+            sensor_send.water_detect_1 = sensor_values[10]
+            sensor_send.water_detect_2 = sensor_values[11]
+            sensor_send.pump = sensor_values[12]
+            rospy.logdebug("Received data from STM32: {}".format(sensor_values))
+        else:
+            rospy.logerr("STM32 read timeout: did not receive any data under a period of {}s".format(timeout))
         stm32_sensors.publish(sensor_send)
         rate.sleep()
     closeSTM32Serial()
--- autosail/src/stm32/stm32_handle.py
+++ autosail/src/stm32/stm32_handle.py
@@ -7,27 +7,34 @@ ser = None
 
 
 def openSTM32Serial(port):
+    global ser
     ser = serial.Serial(port, 52000, timeout=2)  # open serial port
     if ser.isOpen():
         print("STM32 port already open")
+        ser.close()
+        ser.open()
     else:
+        print("Opening STM32 port at: {}".format(port))
         ser.open()
 
 
 def closeSTM32Serial():
+    global ser
     ser.close()
 
 
-def sensor_readings():
+def sensor_readings(timeout):
     """
     Communicating to MCU to acquire sensor  data, from battery life, current consumption and water detection
     :return: data from MCU sensors
     """
+    global ser
     data = b'5'  # data sent
     ser.write(data)  # Serial port write data
+    time_start = time.time()
     while True:
         data = ser.readline()
-        if data != b"":
+        if data != b"" or time.time() - time_start > timeout:
             break
     print("receive data is:", list(data))
     return data
